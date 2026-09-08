b1238145c99d8752f84828b03774504ec01f778a
rei <reoxzoi@outlook.com>
2026-09-08 16:37:58 +0800

    modidy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import './index.css'
 import './framework/theme.css'
 import App from './App.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [{ path: '/', component: App }],
 })
 
